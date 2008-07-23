f9cda283add4718eb6475644019adf41d091a7a2
dmm1 <workspace99@gmail.com>
2008-07-23 13:15:11 +0000

    bugfix function.newsletter_news_items.php

--- Newsletter/pntemplates/plugins/function.newsletter_news_items.php
+++ Newsletter/pntemplates/plugins/function.newsletter_news_items.php
@@ -48,45 +48,32 @@
  * Smarty function to retrieve recent news items
  * @author  Devin Hayes 2004
   * @author ported to zikula by Axel "guite" Guckelsberger 04.07.2008
+   *   <!--[newsletter_newsitems topicid="1" limit_items="2" limit_text="30"]-->
+    *  <!--[newsletter_news_items topicid="1" limit_items="2"]-->
  */
 
-function smarty_function_newsletter_news_items($params, &$smarty) 
+function smarty_function_newsletter_news_items($params, &$smarty)
 {
-    extract($params);
-
-    if(!pnModAvailable('News')){
-        return;
-    }
-
+    if(!pnModAvailable('News')) return;
     pnModDBInfoLoad('News');
-
     $pntable = pnDBGetTables();
-    $stable = $pntable['stories'];
     $scolumn = $pntable['stories_column'];
 
-    $where = $scolumn['ihome'] . '=0';
-    if(isset($topicid) && !empty($topicid)) {
-        $where .= ' AND ' . $scolumn['topic'] . ' = \'' . (int) DataUtil::formatForStore($topicid) . '\'';
+   $where = '';
+    if(isset($params['topicid']) && !empty($params['topicid'])) {
+        $where = $scolumn['topic'] . ' = \'' . (int) DataUtil::formatForStore($params['topicid']) . '\'';
     }
     $orderBy = $scolumn['time'] . ' DESC';
+    $numItems = (isset($params['limit_items']) && is_numeric($params['limit_items'])) ? $params['limit_items'] : 5;
 
-    $numItems = 5;
-    if(isset($limit_items) AND !empty($limit_items)) {
-        $numItems = (int) $limit_items;
-    }
-
-    $items = DBUtil::selectObjectArray('stories');
-
-    if (!count($items)) {
-        return;
-    }
+    $items = DBUtil::selectObjectArray('stories', $where, $orderBy, -1, $numItems);
+    if (!count($items)) return;
 
     $data = array();
-
     foreach($items as $k => $item) {
         $hack = str_replace(array('</p>',"\r\n\r\n","\r\r","\n\n"), '::::', $item['hometext']);
         $story_parts = explode('::::', $hack);
-        if(stristr($story_parts['0'], '<p>')){
+        if(stristr($story_parts['0'], '<p>')) {
             $story_parts['0'] .= '</p>';
         }
 
@@ -96,11 +83,9 @@ function smarty_function_newsletter_news_items($params, &$smarty)
                         'story_content' => $item['hometext']);
     }
 
-
     if (isset($params['assign'])) {
         $smarty->assign($params['assign'], $data);
     } else {
         return $data;
     }
 }
-?>
