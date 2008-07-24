31163f1843056248a7e51852fd0b4a24de7ba2cd
dmm1 <workspace99@gmail.com>
2008-07-24 14:53:52 +0000

    version raise

--- Newsletter/pnversion.php
+++ Newsletter/pnversion.php
@@ -1,6 +1,6 @@
 <?php
 $modversion['name'] = 'Newsletter';
-$modversion['version'] = '1.2.2';
+$modversion['version'] = '1.3';
 $modversion['description'] = 'Automated Newsletter';
 $modversion['changelog'] = 'pndocs/changelog.txt';
 $modversion['credits'] = 'pndocs/credits.txt';
