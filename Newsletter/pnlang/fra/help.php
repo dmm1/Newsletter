2cef40ce87b161efdf263bb43492cc5b65aca1dc
dmm1 <workspace99@gmail.com>
2008-11-20 14:47:56 +0000

    updated french translation merci @ yokav

---
+++ Newsletter/pnlang/fra/help.php
@@ -0,0 +1,5 @@
+<?php
+//Translated by: Gilles Pilloud, Christophe Beaujean aka Yokav, MagicVince
+define('_HELP','Aide');
+define('HELP_SEND_NEWSLETTERS_FROM',' Ceci est l\'adresse e-mail de l\'expéditeur de votre newsletter, par exemple newsletter@yourdomain.com');
+define('_FIND_TEMPLATES','Vous trouverez les modèles dans');
---
+++ Newsletter/pnlang/fra/user.php
@@ -0,0 +1,59 @@
+<?php
+//Translated by: Gilles Pilloud, Christophe Beaujean aka Yokav, MagicVince
+define('_ANON_NOT_ALLOWED','Désolé, vous devez être membre du site pour vous inscrire à la newsletter.');
+define('_APPROVAL_REQUIRED','* abonnements soumis à validation.');
+define('_ARCHIVE_DATE_FORMAT','F jS Y');
+define('_BADAUTHKEY','Clé d\'authentification expirée.');
+define('_ERROR','Une erreur s\'est produite');
+define('_FREQUENCY','Fréquence');
+define('_INVALID_EMAIL','Format d\'adresse e-mail invalide.');
+define('_NEWSLETTER','Newsletter');
+define('_NEWSLETTER_MGMT','Gestion de la newsletter');
+define('_NEWSLETTER_TYPE','Type de newsletter');
+define('_NO_USER','Il n\'y a pas d\'abonnements en attente de votre intervention.');
+define('_NOACCESS','Accès interdit.');
+define('_MISSING_INFO','Veuillez compléter tous les champs.');
+define('_NEWSLETTER','Newsletter');
+define('_NO_ARCHIVE_FOUND','L\'archive demandée n\'a pas été trouvée.');
+define('_NOTIFY_ADMIN_SUBJECT','Abonnement à la Newsletter.');
+define('_NOTIFY_USER_SUBJECT','Abonnement à la Newsletter prise en compte');
+define('_SUBSCRIBE','S\'abonner');
+define('_SUBSCRIBER','Abonné(e)');
+define('_SUBSCRIPTION_FAILED','Votre demande d\'abonnement a échoué. Veuillez essayer à nouveau ou contacter l\'administrateur du site.');
+define('_SUBSCRIPTION_SUCCESSFUL','Merci de vous être abonné(e) à notre newsletter!');
+define('_SUBSCRIPTION_MODIFY_FAILED','Impossible de désactiver le compte.');
+define('_SUBSCRIPTION_MODIFY_SUCCESSFUL','Abonnement mis à jour.');
+define('_UNSUBSCRIBE','Désabonnement');
+define('_UNSUBSCRIBE_ANON','Désolé, vous devez être connecté pour désabonner un membre.');
+define('_UNSUBSCRIBE_FAILED','Impossible de vous désabonner. Veuillez contacter l\'administrateur du site.');
+define('_UNSUBSCRIBE_NO_USER','Aucune demande d\'abonnement nécessitant votre intervention.');
+define('_UNSUBSCRIBE_SUCCESSFUL','Votre abonnement a été supprimé.');
+define('_UNSUBSCRIBE_USER_SUBJECT','Abonnement à la Newsletter annulé.');
+define('_USER_EXISTS','Ce membre existe déjà sur notre site.');
+define('_TERMS_OF_SERVICE','Conditions d\'utilisation');
+define('_TERMS_OF_SERVICE_AGREE','Oui, je suis d\'accord avec les <a href="index.php?module=Newsletter&amp;func=terms_of_service" title="Conditions utilisation">conditions d\'utilisation</a>.');
+define('_TERMS_OF_SERVICE_ERROR','Vous devez accepter les conditions d\'utilisation.');
+define('_VIEWARCHIVES','Afficher les Archives');
+define('_YOUR_EMAIL','Votre adresse e-mail');
+define('_YOUR_NAME','Votre nom');
+
+/* DAYS */
+define('_MONDAY','Lundi');
+define('_TUESDAY','Mardi');
+define('_WEDNESDAY','Mercredi');
+define('_THURSDAY','Jeudi');
+define('_FRIDAY','Vendredi');
+define('_SATURDAY','Samedi');
+define('_SUNDAY','Dimanche');
+
+define('_WEEKLY','hebdomadaire');
+define('_MONTHLY','mensuelle');
+define('_YEARLY','annuelle');
+
+/* NEWSLETTER TYPES */
+define('_TEXT','texte');
+define('_HTML','html');
+define('_TEXTWLINK','texte avec un lien vers l\'archive');
+
+define('_NEWSLETTERSUBSCRIPTIONSVIEW','Newsletter');
+define('_JOIN','Abonnez-vous!');
