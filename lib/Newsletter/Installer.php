c81693637bd7296e15c91049639da4488fe306a3
cmfcmf <cmfcmf.flach@gmail.com>
2013-01-02 13:46:39 +0100

    typo

--- lib/Newsletter/Installer.php
+++ lib/Newsletter/Installer.php
@@ -5,7 +5,7 @@
  * @copyright  Newsletter Team
  * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  * @package    Newsletter
- * @subpackage User
+ * @subpackage Installer
  *
  * Please see the CREDITS.txt file distributed with this source code for further
  * information regarding copyright.
