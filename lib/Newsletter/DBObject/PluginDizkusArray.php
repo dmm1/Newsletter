3ce31545e009e336f476bf0abf612c44d37058ad
N. Petkov <nmpetkov@gmail.com>
2013-02-17 16:16:11 +0200

    General filter implementation for Dizkus

--- lib/Newsletter/DBObject/PluginDizkusArray.php
+++ lib/Newsletter/DBObject/PluginDizkusArray.php
@@ -29,29 +29,35 @@ class Newsletter_DBObject_PluginDizkusArray extends Newsletter_DBObject_PluginBa
         $nItems = ModUtil::getVar ('Newsletter', 'plugin_Dizkus_nItems', 1);
 
         // get all forums the user is allowed to read
-        $userforums = ModUtil::apiFunc('Dizkus', 'user', 'readuserforums');
+        /*$userforums = ModUtil::apiFunc('Dizkus', 'user', 'readuserforums'); 2013-02-17 this function is not working for now, so will replace with the code below */
+        $permFilter = array('component_left' => 'Dizkus',
+            'component_middle' => '',
+            'component_right' => '',
+            'instance_left' => 'cat_id',
+            'instance_middle' => 'forum_id',
+            'instance_right' => '',
+            'level' => ACCESS_READ);
+        $userforums = DBUtil::selectExpandedObjectArray('dizkus_forums', '', '', 'forum_id', -1, -1, 'forum_id', $permFilter);
         if (!is_array($userforums) || count($userforums)==0) {
             // error or user is not allowed to read any forum at all
             // return empty result set without even doing a db access
-            return array($posts, $m2fposts, $rssposts, $text);
+            return array();
         }
 
         // now create a very simple array of forum_ids only. we do not need
         // all the other stuff in the $userforums array entries
         $allowedforums = array();
-        for ($i=0; $i<count($userforums); $i++) {
-            array_push($allowedforums, $userforums[$i]['forum_id']);
+        foreach (array_keys($userforums) as $k) {
+            $allowedforums[] = $userforums[$k]['forum_id'];
         }
-        $whereforum = ' forum_id IN (' . DataUtil::formatForStore(implode($allowedforums, ',')) . ') ';
-
+        $whereforum = ' forum_id IN (' . DataUtil::formatForStore(implode(',', $allowedforums)) . ') ';
 
         $items = DBUtil::selectObjectArray('dizkus_topics', $whereforum, 'topic_id DESC', 0, $nItems);
 
         // filter by date is given, remove older data
-        // ATTENTION: not implemented yet, need other select and lastpost/datetype column
-        if (false && $filtAfterDate) {
+        if ($filtAfterDate) {
             foreach (array_keys($items) as $k) {
-                if ($items[$k]['date'] < $filtAfterDate) {
+                if ($items[$k]['topic_time'] < $filtAfterDate) {
                     unset($items[$k]);
                 }
             }
