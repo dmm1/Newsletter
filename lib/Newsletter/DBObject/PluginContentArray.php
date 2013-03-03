66d5e1a403611610019de04633eb5481ce306362
N. Petkov <nmpetkov@gmail.com>
2013-03-03 22:04:15 +0200

    Content plugin refactored to have "content" and Newsletter user security check

--- lib/Newsletter/DBObject/PluginContentArray.php
+++ lib/Newsletter/DBObject/PluginContentArray.php
@@ -32,25 +32,49 @@ class Newsletter_DBObject_PluginContentArray extends Newsletter_DBObject_PluginB
         if (empty($lang)) {
             $lang = System::getVar('language_i18n', 'en');
         }
+        $dom = ZLanguage::getModuleDomain('Newsletter');
 
-        ModUtil::dbInfoLoad ('content');
-        $table = DBUtil::getTables();
-        $column  = $table['content_page_column'];
-        $where   = "$column[active] = 1";
-        $sort    = "$column[id] DESC";
+        $enableML = ModUtil::getVar('Newsletter', 'enable_multilingual', 0);
         $nItems  = ModUtil::getVar ('Newsletter', 'plugin_Content_nItems', 1);
 
-        $items = DBUtil::selectObjectArray ('content_page', $where, $sort, 0, $nItems);
+        // this can be setting in future
+        // $userNewsletter = 0; this can be default in future, if Zikula core start to accept such parameter in SecurityUtil::checkPermission
+        $userNewsletter = 1; // by default userid=1 is for guest, but it is member of Users group in practice. Better then to chow all forums topics
+
+        $connection = Doctrine_Manager::getInstance()->getCurrentConnection();
+        $sql = "SELECT * FROM content_page WHERE page_active";
+        if ($filtAfterDate) {
+            $sql .= " AND page_cr_date>='".$filtAfterDate."'";
+        }
+        if ($enableML && $lang) {
+            $sql .= " AND (page_language='' OR page_language='".$lang."')";
+        }
+        $sql .= " ORDER BY page_id DESC LIMIT ".$nItems;
+        $stmt = $connection->prepare($sql);
+        try {
+            $stmt->execute();
+        } catch (Exception $e) {
+            return LogUtil::registerError(__('Error in plugin').' Content: ' . $e->getMessage());
+        }
+        $items = $stmt->fetchAll(Doctrine_Core::FETCH_ASSOC);
 
         foreach (array_keys($items) as $k) {
-            if ($filtAfterDate && $items[$k]['cr_date'] < $filtAfterDate) {
-                // filter by date is given, remove older data
+            if (!SecurityUtil::checkPermission('Content:page:', $items[$k]['page_id'].'::', ACCESS_READ, $userNewsletter)) {
                 unset($items[$k]);
             } else {
-                $items[$k]['nl_title'] = $items[$k]['title'];
-                $items[$k]['nl_url_title'] = ModUtil::url('Content', 'user', 'view', array('pid' => $items[$k]['id'], 'newlang' => $lang), null, null, true);
-                //$items[$k]['nl_content'] = $items[$k]['???'];
-                //$items[$k]['nl_url_readmore'] = $items[$k]['nl_url_title'];
+                $items[$k]['nl_title'] = $items[$k]['page_title'];
+                $items[$k]['nl_url_title'] = ModUtil::url('Content', 'user', 'view', array('pid' => $items[$k]['page_id'], 'newlang' => $lang), null, null, true);
+                $items[$k]['nl_url_readmore'] = $items[$k]['nl_url_title'];
+                // get page content array and collect content
+                $items[$k]['nl_content'] = '';
+                $content = ModUtil::apiFunc('Content', 'Content', 'getPageContent', array('pageId' => $items[$k]['page_id'], 'editing' => false, 'translate' => false, 'expandContent' => false));
+                foreach ($content as $area) {
+                    foreach ($area as $arrData) {
+                        if ($arrData['data']['text']) {
+                            $items[$k]['nl_content'] .= ($items[$k]['nl_content'] ? '<br />' : '') . $arrData['data']['text'];
+                        }
+                    }
+                }
             }
         }
 
