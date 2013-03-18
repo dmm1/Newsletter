7006d48b178f409988ae071de357dc21593f9e88
N. Petkov <nmpetkov@gmail.com>
2013-03-18 23:45:58 +0200

    Fixing wrong permission check for News module in api  Account.php

--- lib/Newsletter/Api/Account.php
+++ lib/Newsletter/Api/Account.php
@@ -20,23 +20,11 @@ class Newsletter_Api_Account extends Zikula_AbstractApi
      */
     public function getall($args)
     {
-        $items = array();
-        $uname = (isset($args['uname'])) ? $args['uname'] : UserUtil::getVar('uname');
-
-        // does this user exist?
-        if (UserUtil::getIdFromName($uname) == false) {
-            // user does not exist
-            return $items;
-        }
-
         // Create an array of links to return
-        if (SecurityUtil::checkPermission('News::', '::', ACCESS_COMMENT)) {
-            $items[] = array('url' => ModUtil::url('Newsletter', 'user', 'main'),
-                    'module'  => 'Newsletter',
-                    'title'   => $this->__('Newsletter'),
-                    'icon'    => 'admin.png');
-
-        }
+        $items[] = array('url' => ModUtil::url('Newsletter', 'user', 'main'),
+                'module'  => 'Newsletter',
+                'title'   => $this->__('Newsletter'),
+                'icon'    => 'admin.png');
 
         // Return the items
         return $items;
